d5de9704ec92673e3aa5ed8ab235b850bf0d9cfd
TrongQuyet <trongquyet.nguyen@secomus.com>
2026-04-30 11:42:07 +0700

    2

--- src/org-settings/org-settings.module.ts
+++ src/org-settings/org-settings.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrgSetting } from '../entities/org-setting.entity';
 import { OrgSettingsService } from './org-settings.service';
 import { OrgSettingsController } from './org-settings.controller';
+import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrgSetting])],
+  imports: [TypeOrmModule.forFeature([OrgSetting]), CommonModule],
   controllers: [OrgSettingsController],
   providers: [OrgSettingsService],
 })
